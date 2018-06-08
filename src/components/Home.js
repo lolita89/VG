248c6c68f0a6cfcbc6e8fdab6f9ab464ddf1d8bf
lolita89 <cika.lolita@hotmail.com>
2018-06-08 14:01:57 +0200

    basic setup react, bootstraap, router, component

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                test
+            </div>
+        );
+    }
+}
+
+Home.propTypes = {
+
+};
+
+export default Home;
